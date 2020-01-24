3f55ac95d0bd23719b39dbbbff985cd8ac94502d
euniacke <eric.uniacke@gmail.com>
2020-01-24 10:30:17 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
